1d18df75b8bbe33906506bc259769b2f3510b94b
Matthew Nerger (l) <mnerger@lsu.edu>
2016-03-16 22:31:24 -0500

    Add a food generation function

--- js/snake.js
+++ js/snake.js
@@ -9,7 +9,13 @@ var canvas = document.getElementById('game-area'),
 	ctx = canvas.getContext('2d'),
 //game variables
 	score = 0,
-	level = 0;
+	level = 0,
+	direction = 0;
+//init the map matrix
+var map = new Array(50);
+for (var i = 0; i < map.length; i++) {
+	map[i] = new Array(50);
+}
 
 //functions
 
@@ -30,6 +36,25 @@ function drawMain() {
 	ctx.font = '14px sans-serif';
 	ctx.fillText('Score: ' + score + ' - Level: ' + level, 2, 12);
 }
+
+//generates food for our hungry hero
+function generateFood(map) {
+	//Generates random x and y for food
+	var rndX = Math.round(Math.random() * 19),
+		rndY = Math.round(Math.random() * 19);
+
+	//Assures food doesn't land on the snake's body
+	while (map[rndX][rndY] === 2) {
+		var rndX = Math.round(Math.random() * 19),
+			rndY = Math.round(Math.random() * 19);
+	}
+
+	//places the food out there
+	map[rndX][rndY] = 1;
+	return map;
+}
+
+//canvas manipulation
 
 drawGame();
 
