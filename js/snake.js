e4e6ee19caf397f65dd52d85c325897ab22488aa
Matthew Nerger (l) <mnerger@lsu.edu>
2016-03-16 22:12:08 -0500

    Add drawMain()

--- js/snake.js
+++ js/snake.js
@@ -2,12 +2,28 @@
 	Snake game based in part on tutorial from http://www.andrespagella.com/snake-game
 */
 
-//declare canvas variables
-var canvas = document.getElementById('game-area');
-var ctx = canvas.getContext('2d');
+//global variables
 
-//draw a border around the edge of the canvas element
-ctx.lineWidth = 2;
-ctx.strokeStyle = "black";
-ctx.strokeRect(2, 20, canvas.width - 4, canvas.height - 24);
+//canvas variables
+var canvas = document.getElementById('game-area'),
+	ctx = canvas.getContext('2d'),
+//game variables
+	score = 0,
+	level = 0;
+
+//functions
+
+//draws the frame of the game
+function drawMain() {
+	//draw a border around the edge of the canvas element
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = "black";
+	ctx.strokeRect(2, 20, canvas.width - 4, canvas.height - 24);
+
+	//display the score and level
+	ctx.font = '14px sans-serif';
+	ctx.fillText('Score: ' + score + ' - Level: ' + level, 2, 12);
+}
+
+drawMain();
 
